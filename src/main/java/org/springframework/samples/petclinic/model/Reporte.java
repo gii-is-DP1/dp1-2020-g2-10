1018d016d5cc1edc1d841299599ddd0a0694d052
felixconde35 <felconmar@alum.us.es>
2020-11-15 12:13:07 +0100

    Added ManyToOne to Capitulo in Reporte

--- src/main/java/org/springframework/samples/petclinic/model/Reporte.java
+++ src/main/java/org/springframework/samples/petclinic/model/Reporte.java
@@ -3,6 +3,7 @@ package org.springframework.samples.petclinic.model;
 import java.time.LocalDate;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotEmpty;
 
@@ -23,6 +24,7 @@ public class Reporte extends BaseEntity{
 	private String texto;
 	
 	@NotEmpty
+	@ManyToOne(optional=true)
 	private Capitulo capitulo;
 	
 }
