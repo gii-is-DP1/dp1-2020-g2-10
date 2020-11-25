7493d6cc4c2a0d48727d174614a7fa3e5c5fa1f4
Oscar <oscdoraba@alum.us.es>
2020-11-25 10:56:05 +0100

    H6 - Probada a través de test de servicio pero no implementada en vista (falta la HU de listar/mostrar chapters)

--- src/main/java/org/springframework/samples/petclinic/web/ChapterController.java
+++ src/main/java/org/springframework/samples/petclinic/web/ChapterController.java
@@ -8,20 +8,22 @@ import org.springframework.samples.petclinic.model.Chapter;
 import org.springframework.samples.petclinic.service.AuthorService;
 import org.springframework.samples.petclinic.service.ChapterService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/authores/{authorId}/historias/{historiaId}")
+@RequestMapping("/authors/{authorId}/stories/{storyId}")
 public class ChapterController {
 	
 	private final ChapterService chapterService;
 	// private final HistoriaService historiaService;
 	private final AuthorService authorService;
-	private static final String VISTA_EDICION_chapter= "chapter/editarchapter";
+	private static final String VISTA_EDICION_chapter= "chapters/editChapter";
 	
 	@Autowired
 	public ChapterController(ChapterService chapterService, /*HistoriaService historiaService,*/ AuthorService authorService) {
@@ -35,7 +37,7 @@ public class ChapterController {
 	// HU-05: Añadir un capítulo a una historia.
 	
 	// En el primer método get, mostramos el formulario de edición del nuevo capítulo:
-	@GetMapping("/chapter/new")
+	@GetMapping("/chapters/new")
 	public String añadirchapter(Author author, ModelMap modelMap) {
 		
 		Chapter chapter = new Chapter();
@@ -49,7 +51,7 @@ public class ChapterController {
 	@PostMapping("/chapter/new")
 	public String procesarNuevochapter(Author author, @Valid Chapter cap, BindingResult result, ModelMap modelMap) {
 		
-		String view = "chapter/listadochapters";
+		String view = "chapters/listChapters";
 		
 		// Si al validarlo, encontramos errores:
 		
@@ -69,6 +71,28 @@ public class ChapterController {
 		}
 		
 			return view;
+		}
+	
+	//HU-06 Edición de un capitulo
+		@GetMapping(value = "/chapters/{chapterId}/edit")
+		public String initUpdateChapterForm(@PathVariable("chapterId") int chapterId, Model model) {
+			model.addAttribute("buttonCreate", false); //si esta a false el boton de create no aparecera
+			Chapter chapter = this.chapterService.findChapterById(chapterId);
+			model.addAttribute(chapter);
+			return VISTA_EDICION_chapter;
+		}
+		
+		@PostMapping(value = "/chapters/{chapterId}/edit")
+		public String processUpdateChapterForm(@Valid Chapter chapter, BindingResult result,
+				@PathVariable("chapterId") int chapterId) {
+			if (result.hasErrors()) {
+				return VISTA_EDICION_chapter;
+			}
+			else {
+				chapter.setId(chapterId);
+				this.chapterService.saveChapter(chapter);
+				return "redirect:/chapters/{chapterId}";
+			}
 		}
 	
 }
---
+++ src/test/java/org/springframework/samples/petclinic/service/ChapterServiceTests.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.samples.petclinic.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.samples.petclinic.model.Chapter;
+import org.springframework.samples.petclinic.model.Story;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
+class ChapterServiceTests {        
+    
+	@Autowired
+	protected ChapterService chapterService;
+	
+	@Autowired
+    protected StoryService storyService;
+        
+	@Test
+	@Transactional
+	void shouldUpdateDiagnosis() {
+		// Cambiamos el titulo de diagnosis, y comprobamos que se ha efectuado correctamente el cambio
+		Chapter chapter = this.chapterService.findChapterById(1);
+		String oldTitle = chapter.getTitle();
+		String newTitle = oldTitle + " un cambio cualquiera";
+
+		chapter.setTitle(newTitle); //Cambiamos el titulo
+		
+		this.chapterService.saveChapter(chapter);
+
+		// retrieving new name from database
+		chapter = this.chapterService.findChapterById(1);
+		assertThat(chapter.getTitle()).isEqualTo(newTitle);
+	}
+}
